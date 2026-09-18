1d375a6d045e8b9384c4dcac73883796be766707
Ma. Krizza Casandra Plaza <123443532+KrizzaCasandra@users.noreply.github.com>
2026-09-18 19:50:48 +0800

    Add files via upload
    LAB4

---
+++ EMBEDDED_LAB4/EMBEDDED_LAB4/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace EMBEDDED_LAB4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOn_Click = new System.Windows.Forms.Button();
+            this.btnOff_Click = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.PORT = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnOn_Click
+            // 
+            this.btnOn_Click.BackColor = System.Drawing.Color.Lime;
+            this.btnOn_Click.Location = new System.Drawing.Point(110, 151);
+            this.btnOn_Click.Name = "btnOn_Click";
+            this.btnOn_Click.Size = new System.Drawing.Size(170, 62);
+            this.btnOn_Click.TabIndex = 0;
+            this.btnOn_Click.Text = "ON";
+            this.btnOn_Click.UseVisualStyleBackColor = false;
+            this.btnOn_Click.Click += new System.EventHandler(this.btnOn_Click_Click);
+            // 
+            // btnOff_Click
+            // 
+            this.btnOff_Click.BackColor = System.Drawing.Color.Red;
+            this.btnOff_Click.Location = new System.Drawing.Point(449, 151);
+            this.btnOff_Click.Name = "btnOff_Click";
+            this.btnOff_Click.Size = new System.Drawing.Size(167, 62);
+            this.btnOff_Click.TabIndex = 1;
+            this.btnOff_Click.Text = "OFF";
+            this.btnOff_Click.UseVisualStyleBackColor = false;
+            this.btnOff_Click.Click += new System.EventHandler(this.btnOff_Click_Click);
+            // 
+            // PORT
+            // 
+            this.PORT.Location = new System.Drawing.Point(30, 43);
+            this.PORT.Name = "PORT";
+            this.PORT.Size = new System.Drawing.Size(90, 22);
+            this.PORT.TabIndex = 3;
+            this.PORT.Text = "PORT:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(142, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 250);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.PORT);
+            this.Controls.Add(this.btnOff_Click);
+            this.Controls.Add(this.btnOn_Click);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOn_Click;
+        private System.Windows.Forms.Button btnOff_Click;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox PORT;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ EMBEDDED_LAB4/EMBEDDED_LAB4/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EMBEDDED_LAB4
+{
+    public partial class Form1 : Form
+    {
+        // Stores the time when ON/OFF command was sent
+        private DateTime _commandStart;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+
+            serialPort1.BaudRate = 9600;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.NewLine = "\n";
+
+
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        // COM PORT SELECTION
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Close current port before changing
+                if (serialPort1.IsOpen)
+                    serialPort1.Close();
+
+                if (comboBox1.SelectedItem == null)
+                    return;
+
+                serialPort1.PortName =
+                    comboBox1.SelectedItem.ToString();
+
+                serialPort1.BaudRate = 9600;
+
+                serialPort1.Open();
+
+                this.Text =
+                    $"Connected: {serialPort1.PortName} @ 9600";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Connection error:\n" + ex.Message
+                );
+            }
+        }
+
+
+        // ON BUTTON
+        private void btnOn_Click_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    MessageBox.Show("Serial port is not connected.");
+                    return;
+                }
+
+                // Record when command was sent
+                _commandStart = DateTime.Now;
+
+                // Send ON command
+                serialPort1.Write("1");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Send error:\n" + ex.Message
+                );
+            }
+        }
+
+        // OFF BUTTON
+        private void btnOff_Click_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    MessageBox.Show("Serial port is not connected.");
+                    return;
+                }
+
+                // Record when command was sent
+                _commandStart = DateTime.Now;
+
+                // Send OFF command
+                serialPort1.Write("0");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Send error:\n" + ex.Message
+                );
+            }
+        }
+
+        // RECEIVE ACK FROM ARDUINO
+
+        private void SerialPort1_DataReceived(
+            object sender,
+            SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string line =
+                    serialPort1.ReadLine().Trim();
+
+                if (line == "ACK")
+                {
+                    double ms =
+                        (DateTime.Now - _commandStart)
+                        .TotalMilliseconds;
+
+                    BeginInvoke((MethodInvoker)(() =>
+                    {
+                        this.Text =
+                            $"ACK received - Latency: {ms:F2} ms";
+                    }));
+                }
+            }
+            catch
+            {
+                // Ignore incomplete serial data
+            }
+        }
+
+
+        // CLOSE SERIAL PORT SAFELY
+        protected override void OnFormClosing(
+            FormClosingEventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                    serialPort1.Close();
+            }
+            catch
+            {
+            }
+
+            base.OnFormClosing(e);
+        }
+    }
+}
